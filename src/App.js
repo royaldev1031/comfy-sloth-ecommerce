a5c3fd1f2dafb8716079f0ba6e8eac2428f0e7f5
smartdev0612 <danieldev0612@gmail.com>
2023-01-18 19:07:18 -0800

    create page route

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-
-import styled from 'styled-components'
-
-const Button = styled.button`
-  background: green;
-  color: white;
-`
-
-const Container = styled.div`
-  background: red;
-  color: white;
-  .hero {
-    font-size: 8rem;
-  }
-`
+import {
+  Home,
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from './pages'
 
 function App() {
   return (
-    <div>
-      <h4>This is the first start</h4>
-      <Button>Hello</Button>
-      <Container>
-        <div>
-          <h3>Hello world</h3>
-          <div className="hero">hero text</div>
-        </div>
-      </Container>
-    </div>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   )
 }
 
