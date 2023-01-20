156b833c2eac676029f8eba80101a762c4714d03
smartdev0612 <danieldev0612@gmail.com>
2023-01-19 19:34:50 -0800

    create About, Home pages

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -15,10 +15,12 @@ const AboutPage = () => {
             <div className="underline"></div>
           </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat,
-            voluptatum iure ut veniam perspiciatis fugit reiciendis nam aperiam
-            sint, eius sapiente odio commodi! Praesentium distinctio similique
-            quas repellat sequi cum?
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
+            adipisci debitis magni alias eligendi odit ipsa hic unde sunt
+            provident numquam voluptate sequi perferendis voluptatem placeat
+            magnam, obcaecati quis culpa ullam reiciendis molestias. Amet
+            repudiandae hic culpa architecto nostrum beatae ratione ea,
+            obcaecati ipsam, cupiditate corrupti molestiae quam et unde.
           </p>
         </article>
       </Wrapper>
