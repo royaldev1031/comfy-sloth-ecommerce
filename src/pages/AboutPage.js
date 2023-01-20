f9465c14c3e7681b2997a7f3a75063b89a01c243
smartdev0612 <danieldev0612@gmail.com>
2023-01-19 19:23:28 -0800

    create pagehero,  about, checkout pages

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,26 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat,
+            voluptatum iure ut veniam perspiciatis fugit reiciendis nam aperiam
+            sint, eius sapiente odio commodi! Praesentium distinctio similique
+            quas repellat sequi cum?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
