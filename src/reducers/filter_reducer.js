57c7d9b81e6ad3e70ae8f7ccb17f20278ec0c8f3
smartdev0612 <danieldev0612@gmail.com>
2023-02-03 19:12:57 -0800

    add comment

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -69,11 +69,17 @@ const filter_reducer = (state, action) => {
 
     let tempProducts = [...all_products]
     // filtering
+    // text
     if (text) {
       tempProducts = tempProducts.filter((product) => {
         return product.name.toLowerCase().startsWith(text)
       })
     }
+
+    // category
+
+    // company
+
     return { ...state, filtered_products: tempProducts }
   }
   if (action.type === CLEAR_FILTERS) {
