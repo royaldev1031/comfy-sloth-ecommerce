a02288c4c3ad0097ccf99f9f4214c5a1793aa20c
Smart Dev <danieldev0612@gmail.com>
2023-02-13 18:45:36 -0800

    finish stripe checkout

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -68,7 +68,27 @@ const CheckoutForm = () => {
     setDisabled(event.empty)
     setError(event.error ? event.error.message : '')
   }
-  const handleSubmit = async (ev) => {}
+  const handleSubmit = async (ev) => {
+    ev.preventDefault()
+    setProcessing(true)
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+      },
+    })
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`)
+      setProcessing(false)
+    } else {
+      setError(null)
+      setProcessing(false)
+      setSucceeded(true)
+      setTimeout(() => {
+        clearCart()
+        history.push('/')
+      }, 10000)
+    }
+  }
 
   return (
     <div>
